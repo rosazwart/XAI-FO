52e9b08fa494ca4b0353bb63bbac39411051c439
rosazwart <rosa-zwart@hotmail.com>
2024-08-19 12:35:21 +0200

    Adjusted expl analysis output files

--- analyser/data_params.py
+++ analyser/data_params.py
@@ -1,4 +1,4 @@
-disease = 'oi'
+disease = 'hd'
 
 DATASET_PREFIXES = ['prev', 'restr']
-expl_folders = {DATASET_PREFIXES[0]: 'expl_9', DATASET_PREFIXES[1]: 'expl_8'}
+expl_folders = {DATASET_PREFIXES[0]: 'expl_5', DATASET_PREFIXES[1]: 'expl_all'}
